fbfa08ad65bc14c60950c5a4f62965e102256cbf
RolanZapara <zapara.rolan@gmail.com>
2018-06-28 17:35:50 +0300

    Init commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0813/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task08.task0813;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+20 слов на букву «Л»
+*/
+
+public class Solution {
+    public static void createSet() {
+        //напишите тут ваш код
+        Set<String> set = new HashSet<>();
+        set.add("Лола");
+        set.add("Лила");
+        set.add("Л1ла");
+        set.add("Лйла");
+        set.add("Лцла");
+        set.add("Лула");
+        set.add("Ляла");
+        set.add("Лела");
+        set.add("Лпла");
+        set.add("Лала");
+        set.add("Лвла");
+        set.add("Лыла");
+        set.add("Лфла");
+        set.add("Люла");
+        set.add("Лбла");
+        set.add("Льла");
+        set.add("Лтла");
+        set.add("Лмла");
+        set.add("Лсла");
+        set.add("Лчла");
+        set.containsAll("Л");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0901/Solution.java
@@ -0,0 +1,6 @@
+package com.javarush.task.task09.task0901;
+
+/* 
+Возвращаем StackTrace
+*/
+

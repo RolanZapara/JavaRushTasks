89efbbfbc3b3a76d99013dda1294de52c1bb5194
Rolan Zapara <zapara.rolan@gmail.com>
2022-01-06 11:40:11 +0200

    'Дегустация вин'

--- 2.JavaCore/src/com/javarush/task/task14/task1410/Wine.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1410/Wine.java
@@ -3,5 +3,6 @@ package com.javarush.task.task14.task1410;
 public class Wine extends Drink {
     public String getHolidayName() {
         return "День Рождения";
+        //minor update to see how git works
     }
 }

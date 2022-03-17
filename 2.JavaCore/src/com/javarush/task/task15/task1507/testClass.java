b989ade3459bf4a200fdcaadbf05f0804c4ec7ab
Rolan Zapara <zapara.rolan@gmail.com>
2022-03-17 23:28:33 +0200

    test commit with git stash command

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1507/testClass.java
@@ -0,0 +1,5 @@
+package com.javarush.task.task15.task1507;
+
+public class testClass {
+    //goal - check how works command 'git stash'
+}
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1507/testClass2.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task15.task1507;
+
+public class testClass2 {
+    /*
+    added new class (file) previously did command 'git stash' with class (file) "testClass"
+     */
+}

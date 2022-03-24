f5e5d1c67a4f1657398d75609c6186c672b2ea9f
Rolan Zapara <zapara.rolan@gmail.com>
2022-03-24 13:26:06 +0200

    done task1523 'Перегрузка конструкторов'

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+
+    public Solution() { }
+
+    protected Solution(String s) {
+
+    }
+
+    Solution(String s, int i) {
+
+    }
+
+    private Solution(double i) {
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
